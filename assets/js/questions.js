cdf66e0a09c80a54339510227d9d45a6d987a4fc
khinoj <kristopher.hinojosa@gmail.com>
2023-07-21 21:03:51 -0400

    update

---
+++ assets/js/questions.js
@@ -0,0 +1,27 @@
+var questions = [
+    {
+       header: 'Inside which HTML element do we put the JavaScript?',
+       choices: ['<scripting>', '<javascript>', '<script>', '<js>'],
+       answer: '<script>',
+    },
+    {
+        header: 'Is JavaScript case-sensitive?',
+        choices: ['Yes', 'No'],
+        answer: 'Yes',
+    }, 
+    {
+        header: 'Which operator is used to assign a value to a variable?',
+        choices: ['x', '*', '=', '-'],
+        answer: ['='],
+    },
+    {
+        header: 'Which of the following statements will show a message as well as ask for user input in a popup?',
+        choices: ['alert()', 'prompt()', 'confirm()', 'message()'],
+        answer: 'prompt()',
+    },
+    {
+        header: 'Which of the following is an event listener in JavaScript?',
+        chocies: ['onclick', 'blur', 'click', 'Click()'],
+        answer: 'click',
+    },
+];
